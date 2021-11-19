3ee03268279671d18155d0a4d9cde52b2c3e9b44
Guillaume Patoine <guillaume.patoine@idiv.de>
2021-11-19 22:29:12 +0100

    Initial commit

---
+++ inst/workflow.R
@@ -0,0 +1,11 @@
+
+# File => New Project =>
+
+library(devtools)
+use_description()
+use_namespace()
+use_git()
+use_package("dplyr")
+use_package("leaflet")
+use_package("lavaan")
+use_gpl3_license(name = "Guillaume Patoine")
